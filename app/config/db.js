4bae7b23a2a0de5eedbb90b19bd80830cae69d07
subhadip dey <127054653+subhadip5069@users.noreply.github.com>
2025-04-02 15:07:36 +0530

    Update db.js

--- app/config/db.js
+++ app/config/db.js
@@ -2,13 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("MongoDB connected");
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("✅ MongoDB Connected Successfully!");
     } catch (err) {
-        console.error(err.message);
+        console.error("❌ MongoDB Connection Error:", err.message);
         process.exit(1);
     }
 };
